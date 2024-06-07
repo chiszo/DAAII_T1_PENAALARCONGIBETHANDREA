9cc8d85732694fe9ce7e590da361efa96669abdf
chiszo <gigibethandrea@gmail.com>
2024-06-07 13:50:46 -0500

    agregar bootstrap y js

---
+++ src/main/java/pe/edu/cibertec/DAAII_T1_PENAALARCONGIBETHANDREA/DaaiiT1PenaalarcongibethandreaApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DAAII_T1_PENAALARCONGIBETHANDREA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaaiiT1PenaalarcongibethandreaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaaiiT1PenaalarcongibethandreaApplication.class, args);
+	}
+
+}
